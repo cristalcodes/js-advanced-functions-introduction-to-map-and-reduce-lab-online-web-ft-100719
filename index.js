60cb0d1e7831c76dbcc3ba11f89a1ff2d0eb4148
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-02-06 14:43:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Your code here
+function mapToNegativize(sourceArray){
+  let negativeValues = []
+  for (let i=0; i < sourceArray.length; i++){
+    negativeValues.push( sourceArray[i] * -1)
+  }
+  return negativeValues
+}
+
+function mapToNoChange(sourceArray){
+  let sameValues = []
+  for (let i=0; i < sourceArray.length; i++){
+    sameValues.push(sourceArray[i])
+  }
+  return sameValues
+}
+
+function mapToDouble(sourceArray){
+  let doubledV= []
+  for (let i=0; i < sourceArray.length; i++){
+    doubledV.push(sourceArray[i] * 2)
+  }
+  return doubledV
+}
+
+function mapToSquare(sourceArray){
+  let squaredV= []
+  for (let i=0; i < sourceArray.length; i++){
+    squaredV.push(sourceArray[i]**2)
+  }
+  return squaredV
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  let initial = startingPoint
+  for (let i=0; i < sourceArray.length; i++){
+    initial = sourceArray[i] + initial
+  }
+  return initial
+}
+
+function reduceToAllTrue(sourceArray){
+  for (let i=0; i < sourceArray.length; i++){
+    if (!sourceArray[i]) return false
+  }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray){
+  for (let i=0; i < sourceArray.length; i++){
+    if (!!sourceArray[i]) return true
+  }
+  return false
+}
